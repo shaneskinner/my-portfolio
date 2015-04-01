c15ca5948792865c05ed26af780868968800d365
Shane Skinner <shane@shaneskinner.com>
2015-04-01 13:07:41 -0400

    Made Syntax Updates
    Moved JS to the EOF and updated the meta tags.

--- education.php
+++ education.php
@@ -6,54 +6,32 @@
     File Name:        education.php
     Author:           Shane Skinner
     Date Created:     04-02-2014
-    Last Revised:     05-15-2014
+    Last Revised:     04-01-2015
     Version:          2.0
 -->
 
 <!-- Google Fonts -->
-<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,400italic' rel='stylesheet' 
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,400italic' rel='stylesheet' 
       type='text/css'>
 
 <!-- Meta Data -->
     <meta charset="UTF-8" />
     <meta name="author" content="Shane Skinner" />
     <meta name="description" content="Web Developer Shane Skinner's eResume website." />
-    <meta name="keywords" content="Web, Developer, Designer, Software, Website, HTML, HTML5, CSS, CSS3, JavaScript, JS, 
-                                   JQuery, PHP, WordPress, WP, Domain, Host, eResume, resume, hire, me, job, seeker, 
-                                   freelance, freelancer, ledyard, preston, montville, uncasville, norwich, groton, 
-                                   new london, hartford, connecticut, providence, westerly, coventry, rhode, island, 
-                                   education, certification, college, Southern New Hampshire University" />
-    <meta name="geo.region" content="US-CT" />
-    <meta name="geo.placename" content="Ledyard" />
-    <meta name="geo.position" content="41.4794;-72.0432" />
-    <meta name="ICBM" content="41.4794, -72.0432" />
+    <meta name="keywords" content="Web, Developer, Designer, Software, Website, HTML, HTML5, CSS, CSS3, JavaScript, 
+                                   JS, JQuery, PHP, WordPress, WP, Domain, Host, eResume, resume, hire, me, job, 
+                                   seeker, freelance, freelancer, Cape Cod, Cape, Cod, Hyannis, Barnstable, Fall River, 
+                                   New Bedford, Massachusetts, MA" />
+    <meta name="geo.region" content="US-MA" />
+    <meta name="geo.placename" content="Hyannis" />
+    <meta name="geo.position" content="41.655011;-70.278398" />
+    <meta name="ICBM" content="41.655011, -70.278398" />
 
 <!-- Link to CSS Files -->
     <link href="css/main_styles.css" rel="stylesheet" type="text/css" />
     <!--[if IE]>
       <link href="css/ie.css" rel="stylesheet" type="text/css" /> 
     <![endif]-->
-
-<!-- Link to JavaScript Files -->
-    <script src="js/modernizr.js" type="text/javascript"></script>
-    <script src="js/stringReverse.js" type="text/javascript"></script>
-    <script src="js/showEm.js" type="text/javascript"></script>
-
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-
-    <script type="text/javascript">
-      jQuery(document).ready(function($){
-
-        /* prepend menu icon */
-        $('.nav-wrap').prepend('<div id="menu-icon">Menu</div>'); 
-
-        /* toggle nav */
-        $("#menu-icon").on("click", function(){
-          $(".nav").slideToggle();
-          $(this).toggleClass("active");
-        });
-      });
-    </script>
 
     <title>Shane Skinner's eResume</title>
   </head>
@@ -111,5 +89,26 @@
     <footer>
       <?php include "php/footer.php";?>
     </footer>
+
+<!-- Link to JavaScript Files -->
+    <script src="js/modernizr.js" type="text/javascript"></script>
+    <script src="js/stringReverse.js" type="text/javascript"></script>
+    <script src="js/showEm.js" type="text/javascript"></script>
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+
+    <script type="text/javascript">
+      jQuery(document).ready(function($){
+
+        /* prepend menu icon */
+        $('.nav-wrap').prepend('<div id="menu-icon">Menu</div>'); 
+
+        /* toggle nav */
+        $("#menu-icon").on("click", function(){
+          $(".nav").slideToggle();
+          $(this).toggleClass("active");
+        });
+      });
+    </script>
   </body>
 </html>
